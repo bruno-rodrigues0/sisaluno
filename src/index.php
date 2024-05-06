6e270a6e5a9c77674ec471b1efbc45346aaa2fa2
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-05-06 18:31:27 -0300

    Initial Commit

---
+++ src/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema aluno</title>
+    <link rel="shortcut icon" href="./../public/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./globals.css">
+    <link rel="stylesheet" href="./styles/index.css">
+</head>
+
+<body>
+    <main>
+        <h1>Sisaluno</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Aluno</a>
+                </li>
+                <li>
+                    <a href="./routes/professor.php">Professor</a>
+                </li>
+            </ul>
+        </nav>
+    </main>
+</body>
+
+</html>
---
+++ src/routes/professor.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema aluno</title>
+    <link rel="shortcut icon" href="./../../public/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./styles/index.css">
+</head>
+
+<body>
+    <header>
+        <h1>Sistema aluno</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="">Inicio</a>
+                </li>
+                <li>
+                    <a href="">Aluno</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        <nav>
+            <ul>
+                <li>
+                    <a href="">Inicio</a>
+                </li>
+                <li>
+                    <a href="">Aluno</a>
+                </li>
+            </ul>
+        </nav>
+
+        <span> Copyright &copy; 2024</span>
+    </footer>
+</body>
+
+</html>
