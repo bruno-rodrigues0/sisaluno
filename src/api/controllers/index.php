d6fa671ad9d153c6990e274dfd8045e989065859
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-05-07 11:22:19 -0300

    arquivos de controller

--- src/api/controllers/index.php
+++ src/api/controllers/index.php
@@ -2,4 +2,6 @@
 
 require_once realpath(__DIR__ . "/professor/index.php");
 require_once realpath(__DIR__ . "/aluno/index.php");
+require_once realpath(__DIR__ . "/turma/index.php");
+require_once realpath(__DIR__ . "/disciplinas/index.php");
 
