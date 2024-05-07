c0792ebc90b77785298fadb99fd4a5ad97191cdd
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-05-07 19:33:11 -0300

    favicon arrumado

--- src/routes/aluno.php
+++ src/routes/aluno.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aluno | Sistema aluno</title>
-    <link rel="shortcut icon" href="./../../../public/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="./../../public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="./../globals.css"> 
 </head>
 
--- src/routes/professor.php
+++ src/routes/professor.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Professor | Sistema aluno</title>
-    <link rel="shortcut icon" href="./../../../public/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="./../../public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="./../globals.css">
     <link rel="stylesheet" href="./../styles/professor.css">
 </head>
